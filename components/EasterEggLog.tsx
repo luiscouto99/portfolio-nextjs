843e63bf69d78c87fe76624530ab0281fb08d038
Luis Couto <luis.couto@mindswap.academy>
2022-10-30 20:34:49 +0000

    easter egg log changed

--- components/EasterEggLog.tsx
+++ components/EasterEggLog.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 
 const EasterEggLog = () => {
     useEffect(() => {
-        console.log("%cI see you are a curious one... \nYou are one step closer from discovering the easter egg. \nClick on my name in the <header>, 5 times, to see some magic happening ;)", "color: #66D99B; background-color: #171717; font-size: 18px; ")
+        console.log("%cI see you are a curious one... \nCongrats though! You are now one step closer from findind the easter egg. \nClick on my name 5 times, to see some magic happening ;)", "color: #66D99B; background-color: #171717; font-size: 18px; ")
     }, []);
     
   return <></>
