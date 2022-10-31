e0bdaa4051a8dae64c2f31afcce8dde2c7bdc145
Luis Couto <luis.couto@mindswap.academy>
2022-10-31 11:25:44 +0000

    responsive easter egg

--- components/EasterEgg.tsx
+++ components/EasterEgg.tsx
@@ -1,43 +1,49 @@
 import React from 'react'
 import Sketch from "react-p5";
 import p5Types from "p5";
-import { useWindowWidth } from '../hooks/useWindowWidth';
-import { useWindowHeight } from '../hooks/useWindowHeight';
 
 let video: any;
 const EasterEgg = () => {
-    const screenWidth = useWindowWidth();
-    const screenHeight = useWindowHeight();
 
     const setup = (p5: p5Types, canvasParentRef: Element) => {
-        p5.createCanvas(screenWidth, screenHeight).parent(canvasParentRef);
-
+        p5.createCanvas(p5.windowWidth, p5.windowHeight - 5).parent(canvasParentRef);
+        p5.resizeCanvas(p5.windowWidth, p5.windowHeight - 5);
         video = p5.createCapture(p5.VIDEO);
         video.size(p5.width, p5.height);
         video.hide();
     };
+
+    function windowResized(p5: p5Types) {
+        p5.resizeCanvas(p5.windowWidth, p5.windowHeight - 5);
+        video.size(p5.width, p5.height);
+    }
 
     const draw = (p5: p5Types) => {
         p5.background(255);
         p5.translate(p5.width, 0);
         p5.scale(-1, 1);
 
-        let gridSize = p5.int(p5.map(1500, p5.width, 0, 5, 50));
-        video.loadPixels();
-        for (let y = 0; y < p5.height; y += gridSize) {
-            for (let x = 0; x < p5.width; x += gridSize) {
+        let gridSize = p5.int(p5.map(0, p5.width, 0, p5.height, p5.width));
+        let offSet = Math.floor(gridSize / 100);
 
+        if (offSet < 8) {
+            offSet = 8
+        };
+
+        video.loadPixels();
+        
+        for (let y = 0; y < p5.height; y += offSet) {
+            for (let x = 0; x < p5.width; x += offSet) {
                 let index = (y * p5.width + x) * 4;
                 let r = video.pixels[index];
-                let dia = p5.map(r, 0, 255, gridSize, 2);
-
+                let circleSize = p5.map(r, 0, 255, offSet, 2);
                 p5.fill(0);
                 p5.noStroke();
-                p5.circle(x + gridSize / 2, y + gridSize / 2, dia);
+                p5.circle(x + offSet / 2, y + offSet / 2, circleSize);
             }
         }
     };
-    return <Sketch setup={setup} draw={draw} />;
+    return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
 }
 
 export default EasterEgg
