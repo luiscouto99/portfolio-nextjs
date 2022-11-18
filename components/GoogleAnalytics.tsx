76d125e2d8508aba98ba6de33a55235cda4f015c
Luis Couto <luis.couto@mindswap.academy>
2022-11-18 12:28:07 +0000

    adding google analytics tracker

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Script from 'next/script';
+
+const GoogleAnalytics = () => {
+    return (
+        <>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-J6NLE74E3M" strategy='afterInteractive' />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-J6NLE74E3M');
+                `}
+            </Script>
+        </>
+    )
+}
+
+export default GoogleAnalytics
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import PageLoader from '../components/PageLoader';
 import { useState } from 'react';
+import GoogleAnalytics from '../components/GoogleAnalytics';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isLoading, setIsLoading] = useState(true);
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       {isLoading && <PageLoader loader={loader} layout={layout} />}
       <Layout isLoading={layoutLoading}>
+        <GoogleAnalytics />
         <Component {...pageProps} />
       </Layout>
     </>
