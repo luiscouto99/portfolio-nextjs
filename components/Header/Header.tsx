5c7f673cae1b9c4226f3ac38a51f37eb9ae4431f
Luis Couto <luis.couto@mindswap.academy>
2022-11-26 12:06:30 +0000

    removed dead code in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -21,29 +21,10 @@ const Header = ({
   const [time, setTime] = useState('');
   const [counter, setCounter] = useState(0);
 
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     const date = new Date();
-  //     const hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();
-  //     const minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
-
-  //     setTime(hours + ':' + minutes + ` ${date.getHours() < 12 ? 'AM' : 'PM'}`);
-  //   }, 1000);
-  // }, [time]);
-
-  // useEffect(() => {
-  //   const getTime = async () => {
-  //     const timeLondon = await getLondonTime();
-  //     setTime(timeLondon);
-  //   };
-  //   getTime();
-  // }, []);
-
   useEffect(() => {
     const getTime = async () => {
       const timeInLondon = await getLondonTime();
       setTime(timeInLondon);
-      console.log('logged time');
     };
 
     const timeInterval = setInterval(() => {
